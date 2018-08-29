8c952a17e4f23f591bb68b9fd3e47592d00be6b8
Alexander Stepanov <al3xst@gmail.com>
2018-08-29 20:22:35 +0200

    Added some more member vars

--- src/pomodoro_timer.cpp
+++ src/pomodoro_timer.cpp
@@ -1,24 +1,52 @@
 #include "pomodoro_timer.h"
 
-PomodoroTimer::PomodoroTimer() {
-	menu.addAction(&startstop);
-	menu.addSeparator();
-	menu.addAction(&quitaction);
+#include <QDebug>
 
-	QObject::connect(&startstop, &QAction::triggered, [=](){this->toggle();});
-	QObject::connect(&quitaction, &QAction::triggered, [=](){QApplication::quit();});
+PomodoroTimer::PomodoroTimer() {
+	actions["startstop"].setText("Toggle Start/Stop");
+	actions["restartcurr"].setText("Restart current Pomodoro");
+	actions["skip"].setText("Skip current Pomodoro");
+	actions["restart"].setText("Restart all");
+	actions["extend"].setText("Extend current timer");
+	actions["exit"].setText("Exit");
+
+	menu.addAction(&actions["startstop"]);
+	menu.addAction(&actions["restartcurr"]);
+	menu.addAction(&actions["skip"]); //TODO
+	//menu.addAction(&actions["restart"]); //TODO
+	//menu.addAction(&actions["extend"]); //TODO
+	menu.addSeparator();
+	menu.addAction(&actions["exit"]);
+
+	QObject::connect(&actions["startstop"], &QAction::triggered, [=](){this->toggle();}); //"Toggle Start/Stop"
+	QObject::connect(&actions["restartcurr"], &QAction::triggered, [=](){this->timer.start();}); //"Restart current Pomodoro"
+	QObject::connect(&actions["exit"], &QAction::triggered, [=](){QApplication::quit();}); //"Exit"
+
+	QObject::connect(&sti, &QSystemTrayIcon::activated, [=](QSystemTrayIcon::ActivationReason reason){
+		actions["skip"].setText(std::to_string(timer.remainingTime()).c_str());
+	});
+
+	timer.setInterval(pomodoro_time * 1000);
+
+	QObject::connect(&timer, &QTimer::timeout, [=](){
+		this->sti.showMessage("Timeout", "*ring* *ring*");
+		timer.start(pomodoro_time * 1000);
+	});
 
 	sti.setContextMenu(&menu);
 	sti.setIcon(icon_paused);
 	sti.show();
-	sti.showMessage("TEST", "Hello World");
+	//sti.showMessage("TEST", "Hello World");
 }
 
 void PomodoroTimer::toggle() {
 	if(running) {
 		sti.setIcon(icon_paused);
+		timer_remaining = timer.remainingTime();
+		timer.stop();
 	} else {
 		sti.setIcon(icon_running);
+		timer.start(timer_remaining);
 	}
 	running = !running;
 }
--- src/pomodoro_timer.h
+++ src/pomodoro_timer.h
@@ -7,7 +7,11 @@
 #include <QMenu>
 #include <QObject>
 #include <QSystemTrayIcon>
+#include <QTimer>
 #include <QWidget>
+
+#include <map>
+#include <string>
 
 class PomodoroTimer {
 public:
@@ -17,13 +21,25 @@ public:
 	void toggle();
 
 private:
-	double time{0.0};
+	int pomodoro_time{1500}; //in seconds. 1500 = 25minutes
+	int pomodoro_pause{300}; //in seconds. 300 = 5minutes
+	int pomodoro_big_pause{1200}; //in seconds. 1200 = 20minutes
+	int pomodoros{3}; //pomodoros until bigger pause
+	int pomodoro_cntr{0}; //current pomodoro intervall counter
+
+	int timer_remaining;
+
 	bool running{false};
 
-	QSystemTrayIcon sti;
+	std::map<std::string, QAction> actions;
+
+	//QAction startstop{"Toggle Start/Stop"};
+	//QAction quitaction{"Exit"};
 	QMenu menu;
-	QAction startstop{"Toggle Start/Stop"};
-	QAction quitaction{"Exit"};
+
+	QSystemTrayIcon sti;
+
+	QTimer timer;
 
 	//show somewhere
 	//designed by Freepik from Flaticon
